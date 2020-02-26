2f84b47bb4e0a6f48b10f02b4ab85197864a49e7
Shingo Sato <shinsugar@gmail.com>
2020-02-26 15:00:24 +0900

    Fix to use @actions/github

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,14 @@
+import * as fs from 'fs';
 import * as core from '@actions/core';
+import { GitHub, context } from '@actions/github';
 import { uniq, includesOneof } from './utils';
-
-type LabelsItem = {
-  color: string;
-  default: boolean;
-  description: string;
-  id: number;
-  name: string;
-  node_id: string;
-  url: string;
-};
-
-type GitHubEvent = {
-  pull_request: {
-    labels: LabelsItem[];
-  };
-}
 
 export async function run(): Promise<void> {
   try {
-    const requiredAny = core.getInput('required_any');
-    const requiredAll = core.getInput('required_all');
-    const requiredOneof = core.getInput('required_oneof');
-    const banned = core.getInput('banned');
+    const requiredAny = core.getInput('required_any', { required: false });
+    const requiredAll = core.getInput('required_all', { required: false });
+    const requiredOneof = core.getInput('required_oneof', { required: false });
+    const banned = core.getInput('banned', { required: false });
 
     if (!requiredAny && !requiredAll && !requiredOneof && !banned) {
       console.log('nothing labels to check');
@@ -30,13 +16,28 @@ export async function run(): Promise<void> {
     }
 
     const { GITHUB_EVENT_PATH } = process.env;
-
     if (!GITHUB_EVENT_PATH) {
       throw new Error('`GITHUB_EVENT_PATH` must be required');
     }
 
-    const event = require(GITHUB_EVENT_PATH) as GitHubEvent;
-    const prLabelNames = event.pull_request.labels.map(l => l.name);
+    const token = core.getInput('github_token', { required: true });
+    const client = new GitHub(token);
+
+    const event = JSON.parse(
+      fs.readFileSync(GITHUB_EVENT_PATH, 'utf8')
+    ) as { pull_request?: { number?: number }};
+
+    if (!event.pull_request || !event.pull_request.number) {
+      throw new Error('there is no pull request data in event');
+    }
+
+    const { data: pullRequest } = await client.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: event.pull_request.number, // eslint-disable-line @typescript-eslint/camelcase
+    });
+
+    const prLabelNames = pullRequest.labels.map(l => l.name);
 
     if (requiredAny) {
       const requiredAnyLabels = uniq<string>(requiredAny.split(',').filter(l => l));
